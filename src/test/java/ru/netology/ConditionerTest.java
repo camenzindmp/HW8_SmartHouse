7e5e95ae7a76b3c084c9394ec05c334d7d3e128e
Maksim <camenzindmp@gmail.com>
2020-05-30 22:27:57 +1000

    more tests added

--- src/test/java/ru/netology/ConditionerTest.java
+++ src/test/java/ru/netology/ConditionerTest.java
@@ -7,22 +7,47 @@ import static org.junit.jupiter.api.Assertions.*;
 class ConditionerTest {
 
     @Test
-    void increaseCurrentTemperature() {
+    void setCurrentTemperature() {
+        Conditioner conditioner = new Conditioner();
+        conditioner.setMaxTemperature(25);
+        conditioner.setMinTemperature(-12);
+        assertEquals(0, conditioner.getCurrentTemperature());
+        conditioner.setCurrentTemperature(-100);
+        assertEquals(0, conditioner.getCurrentTemperature());
+    }
+
+
+    @Test
+    void increaseCurrentTemperature() {          //вызов метода должен повышать t на 1;
         Conditioner conditioner = new Conditioner();
         conditioner.setMaxTemperature(20);
-        conditioner.setCurrentTemperature(19);
         conditioner.increaseCurrentTemperature();
-        int expected = 20;
-        assertEquals(expected, conditioner.getCurrentTemperature());
+        assertEquals(1, conditioner.getCurrentTemperature());
     }
 
     @Test
-    void decreaseCurrentTemperature() {
+    void decreaseCurrentTemperature() {           //вызов метода должен понижать t на 1;
         Conditioner conditioner = new Conditioner();
         conditioner.setMinTemperature(-20);
-        conditioner.setCurrentTemperature(-19);
         conditioner.decreaseCurrentTemperature();
-        int expected = -20;
-        assertEquals(expected, conditioner.getCurrentTemperature());
+        assertEquals(-1, conditioner.getCurrentTemperature());
+    }
+
+    @Test
+    void increaseCurrentTemperatureUpperBound() {    //попытка выйти за границей верхнего значения;
+        Conditioner conditioner = new Conditioner();
+        conditioner.setMaxTemperature(20);
+        conditioner.setCurrentTemperature(20);
+        conditioner.increaseCurrentTemperature();
+        assertEquals(20, conditioner.getCurrentTemperature());
+    }
+
+    @Test
+    void decreaseCurrentTemperatureLowerBound() {     //попытка выйти за границы нижнего значения;
+        Conditioner conditioner = new Conditioner();
+        conditioner.setMinTemperature(-20);
+        conditioner.setCurrentTemperature(-20);
+        conditioner.decreaseCurrentTemperature();
+        assertEquals(-20, conditioner.getCurrentTemperature());
     }
 }
